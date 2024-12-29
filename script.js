79d8476c0f39ba21c9138827af6f2f2ae1b7b38c
M Ganesh123 <work.ganesh67@gamil.com>
2024-12-29 14:41:32 +0530

    some error fix

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let row = document.querySelector(".datarow");
 let secondrow = document.querySelector(".seconddatarow");
 let thardrow = document.querySelector(".tharddatarow");
 // image slider js
-console.log(arr);
 
 const swiper = new Swiper(".slider-wrapper", {
   loop: true,
@@ -70,25 +69,23 @@ const fetchImages = async (query, value) => {
   const options = {
     method: "GET",
     headers: {
-      "x-rapidapi-key": "a82b059de3msh754927c6605ffc7p10ddb9jsn12f49777213a",
+      "x-rapidapi-key": "b6329641a6msh50f569605641ffep17c147jsn4591de643b27",
       "x-rapidapi-host": "real-time-amazon-data.p.rapidapi.com",
     },
   };
   try {
     const response = await fetch(url, options);
     const result = await response.json();
-    console.log(result);
     row.innerHTML = "";
     secondrow.innerHTML = "";
     thardrow.innerHTML = "";
 
     if (result) {
       result.data.products.forEach((e) => {
-        console.log(e);
-        console.log("hiiii");
         const col_lg_3 = document.createElement("div");
         col_lg_3.classList.add("col-lg-3");
         col_lg_3.classList.add("col-md-4");
+        col_lg_3.classList.add("px-0");
         col_lg_3.classList.add("col-6");
         const box = document.createElement("div");
         box.classList.add("box");
@@ -101,7 +98,7 @@ const fetchImages = async (query, value) => {
             : e.product_title;
 
         content.innerHTML = ` <h4>${title}</h4>  
-                    <h3>${e.product_price}</h3>`;
+                    <h3>${e.product_price} <i class="fa-solid fa-bag-shopping"></i></h3> `;
         box.appendChild(content);
 
         col_lg_3.appendChild(box);
@@ -113,119 +110,9 @@ const fetchImages = async (query, value) => {
       });
     }
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
 };
-// const fetchImages = async (query) => {
-//   try {
-//     console.log(query);
-//     const url = `https://real-time-amazon-data.p.rapidapi.com/search?query=${query}&page=1&country=IN&sort_by=RELEVANCE&product_condition=ALL&is_prime=false&deals_and_discounts=NONE`;
-//     const options = {
-//       method: "GET",
-//       headers: {
-//         "x-rapidapi-key": "a82b059de3msh754927c6605ffc7p10ddb9jsn12f49777213a",
-//         "x-rapidapi-host": "real-time-amazon-data.p.rapidapi.com",
-//       },
-//     };
-
-//     try {
-//       const response = await fetch(url, options);
-//       const result = await response.json();
-//       console.log(result);
-//       const products = result.seller_products;
-//       if (result.data.length > 0) {
-//         result.data.forEach((photo) => {
-//           const col_lg_3 = document.createElement("div");
-//           col_lg_3.classList.add("databox");
-//           const box = document.createElement("div");
-//           box.classList.add("box");
-//           const content = document.createElement("div");
-//           content.classList.add("content");
-//           // const title = document.createElement("h4");
-//           // const price = document.createElement("h3");
-//           // title.classList.add("title");
-//           box.innerHTML = `<img src="${result.data.products.product_photo}"/>`;
-//           content.innerHTML = ` <h4>${result.data.products.product_title}</h4>
-//                     <h3>$2500</h3>`;
-//           box.appendChild(content);
-
-//           col_lg_3.appendChild(box);
-//           row.appendChild(col_lg_3);
-
-//           // const overlayElement = document.createElement("div");
-//           // overlayElement.classList.add("overlay");
-
-//           // const overlayText = document.createElement("h3");
-//           // overlayText.innerText = `${photo.alt_description}`;
-
-//           // overlayElement.appendChild(overlayText);
-//           // imageElement.appendChild(overlayElement);
-//           // imagesContainer.appendChild(imageElement);
-//         });
-
-//         // if (data.total_pages === pageNo) {
-//         //   loadMoreBtn.style.display = "none";
-//         // } else {
-//         //   loadMoreBtn.style.display = "block";
-//         // }
-//       }
-//       // for (let i = 0; i < 35; i++) {
-//       //   const title = result.data.products[i].product_title;
-//       //   const price = result.data.products[i].product_price;
-//       //   const photo_az = result.data.products[i].product_photo;
-
-//       //   console.log(`Title:${title}, price : ${price}, photo : ${photo_az}`);
-//       // }
-//     } catch (error) {
-//       console.error(error);
-//     }
-
-//     console.log(result);
-
-//     if (result.data.length > 0) {
-//       results.data.forEach((photo) => {
-//         const col_lg_3 = document.createElement("div");
-//         col_lg_3.classList.add("col-lg-3");
-//         const box = document.createElement("div");
-//         box.classList.add("box");
-//         const content = document.createElement("div");
-//         content.classList.add("content");
-//         const title = document.createElement("h4");
-//         const price = document.createElement("h3");
-//         title.classList.add("title");
-//         box.innerHTML = `<img src="${photo.urls.regular}"/>`;
-//         content.innerHTML = ` <h4>title</h4>
-//                   <h3>$2500</h3>`;
-//         box.appendChild(content);
-
-//         col_lg_3.appendChild(box);
-//         row.appendChild(col_lg_3);
-//         // const overlayElement = document.createElement("div");
-//         // overlayElement.classList.add("overlay");
-
-//         // const overlayText = document.createElement("h3");
-//         // overlayText.innerText = `${photo.alt_description}`;
-
-//         // overlayElement.appendChild(overlayText);
-//         // imageElement.appendChild(overlayElement);
-//         // imagesContainer.appendChild(imageElement);
-//       });
-
-//       // if (data.total_pages === pageNo) {
-//       //   loadMoreBtn.style.display = "none";
-//       // } else {
-//       //   loadMoreBtn.style.display = "block";
-//       // }
-//     } else {
-//       imagesContainer.innerHTML = `<h2>No image found.</h2>`;
-//       if (loadMoreBtn.style.display === "block") {
-//         loadMoreBtn.style.display = "none";
-//       }
-//     }
-//   } catch (error) {
-//     // imagesContainer.innerHTML = `<h2>failed to fetch images. plese try again later.</h2>`;
-//   }
-// };
 
 searchform.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -233,7 +120,5 @@ searchform.addEventListener("submit", (e) => {
   if (inputText !== "") {
     page = 1;
     fetchImages(inputText);
-  } // else {
-  //   imagesContainer.innerHTML = `<h2>Please enter a search query.</h2>`;
-  // }
+  }
 });
